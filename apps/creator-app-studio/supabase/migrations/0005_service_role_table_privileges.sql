ebff57e13a4a2a7765437d9f8a5ac24b112d4c7e
Cameron Winders <cameronwinders@gmail.com>
2026-04-28 14:07:45 -0500

    Grant creator app studio service role table access

---
+++ apps/creator-app-studio/supabase/migrations/0005_service_role_table_privileges.sql
@@ -0,0 +1,3 @@
+grant all privileges on app_creator_app_studio.leads to service_role;
+grant all privileges on app_creator_app_studio.accounts to service_role;
+grant all privileges on app_creator_app_studio.admin_allowlist to service_role;
